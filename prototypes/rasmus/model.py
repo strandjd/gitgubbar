0da01c79c768bc59d90b2c8092381858e33507f1
Nezcobar <hajt@live.se>
2022-08-22 16:40:38 +0200

    added comments to model.py

--- prototypes/rasmus/model.py
+++ prototypes/rasmus/model.py
@@ -4,51 +4,63 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 
+# storleken som bilderna ska omvandlas till i pixlar (både längd och bredd) 
 BASE_SIZE = 224
 
+# dir där datasettet ligger 
 DATASET_DIR = os.path.join(
     os.path.abspath(os.path.join(os.path.dirname(__file__),"..")), 'emotions')
 
 
+# funktion för att ladda bilderna från datasetkällan till variablerna X och y
 def load_category(category):
-    cnt = 0
-
+    # directory där kategorins bilder finns
     dir = os.path.join(DATASET_DIR, category)
 
+    # för varje bild med angiven kategori
     for img in os.listdir(dir):
+        # ladda bilden i variablen <pic> om möjligt. annars skippa
         try:
             pic = Image.open(f'{dir}/{img}').convert('L')
         except:
             print(f'{img} not found')
             continue
+
+        # ändra till angiven storlek
         pic = pic.resize((BASE_SIZE, BASE_SIZE), Image.ANTIALIAS)
+        # gör pixeldatan till 1-dimensionell
         arr = np.asarray(pic).flatten()
 
+        # fyll på i globala variablerna X och y
         X.append(arr)
         y.append(category)
-        
+
+# globala variabler X för data, y för labels
 X = []
 y = []
 
-load_category('happiness')
-load_category('sadness')
-# load_category('neutrality')
-# load_category('fear')
-# load_category('surprise')
+if __name__=='__main__':
 
-# print(len(X), len(y))
+    # laddar data från följande kategorier
+    load_category('happiness')
+    load_category('sadness')
 
-clf = KNeighborsClassifier(n_neighbors=3)
+    # skapar en estimator. (Denna kan bytas ut till andra algoritmer)
+    clf = KNeighborsClassifier(n_neighbors=3)
 
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.25, shuffle=True)
+    # hämtar ut tränings- och test-data
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.25, shuffle=True)
+  
+    print('fitting')
+    clf.fit(X, y)
 
-print('fitting')
-clf.fit(X, y)
-print('predicting')
-pred = clf.predict(X_test)
-res = [True for i, j in zip(pred, y_test) if i == j]
+    print('predicting')
+    pred = clf.predict(X_test)
 
-print(f'predictions: {len(pred)}. dataset size: {len(X)}')
+    # antalet korrekta predictions lagras i variablen res
+    res = len([True for i, j in zip(pred, y_test) if i == j])
 
-print(f'{int((len(res)/len(y_test)) * 100)}%')
+    # printar ut lite statistik
+    print(f'predictions: {len(pred)}. dataset size: {len(X)}')
+    print(f'{int((res/len(y_test)) * 100)}%')
