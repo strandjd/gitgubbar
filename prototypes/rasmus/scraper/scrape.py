6c1aefb2eb8cb90c6ef28bc750e4c351cc055c92
Nezcobar <hajt@live.se>
2022-08-24 16:32:27 +0200

    scraper download function works

--- prototypes/rasmus/scraper/scrape.py
+++ prototypes/rasmus/scraper/scrape.py
@@ -1,6 +1,7 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-import os, json, time
+import os, json, time, requests
+from googlesearch import search
 
 SCRAPER_DIR = os.path.abspath(os.path.dirname(__file__))
 
@@ -8,7 +9,9 @@ SCRAPED_IMAGES_FILE = os.path.join(SCRAPER_DIR, 'scraped_images.json')
 EXPLORED_URLS_FILE  = os.path.join(SCRAPER_DIR, 'explored_urls.json')
 TO_EXPLORE_FILE     = os.path.join(SCRAPER_DIR, 'to_explore.json')
 
-BASE_URL = 'https://en.wikipedia.org/wiki/Main_Page'
+GOOGLE_QUERY_URL = 'https://www.google.se/search?q='
+RANDOM_WORD_API_URL = 'https://random-word-api.herokuapp.com/word'
+BACKUP_BASE_URLS = ['https://www.thetimes.co.uk/', 'https://www.hd.se/sverige', 'https://en.wikipedia.org/wiki/Main_Page']
 
 class Scraper():
     def __init__(self):
@@ -25,7 +28,7 @@ class Scraper():
         
         if not os.path.isfile(TO_EXPLORE_FILE):
             fp = open(TO_EXPLORE_FILE, 'w')
-            json.dump([BASE_URL], fp=fp, indent=4)
+            json.dump([], fp=fp, indent=4)
             fp.close()
 
 
@@ -41,6 +44,37 @@ class Scraper():
         self.to_explore = set(json.load(fp))
         fp.close()
 
+    def download_scraped_images(self, dirout):
+        if len(self.scraped_images) == 0:
+            return False
+        if not os.path.isdir(dirout):
+            return False
+
+        for i, image in enumerate(self.scraped_images):
+            try:
+                img_data = requests.get(image['image_url']).content
+                with open(os.path.join(dirout, f"{i}.jpg"), 'wb') as fp:
+                    fp.write(img_data)
+            except Exception as e:
+                continue
+
+        return True
+
+
+    def load_random_urls(self): 
+
+        try:
+            #get random word
+            word = requests.get(RANDOM_WORD_API_URL).json()[0] 
+
+            #get google queries
+            for url in search(GOOGLE_QUERY_URL + word, num=5, stop=10, pause=5):
+                if url not in self.explored_urls:
+                    self.to_explore.add(url)
+        except:
+            self.to_explore = set(BACKUP_BASE_URLS)
+
+
     def save(self):
         fp = open(EXPLORED_URLS_FILE, 'w')
         json.dump(list(self.explored_urls), fp, indent=4)
@@ -55,6 +89,10 @@ class Scraper():
         fp.close()
 
     def run(self):
+
+        if len(self.to_explore) == 0:
+            self.load_random_urls()
+
         url = self.to_explore.pop()
         self.explored_urls.add(url)
         try:
@@ -66,20 +104,37 @@ class Scraper():
         urls = soup.find_all('a', href=True)
 
         for item in images:
-            if item['src'] not in [x['image_url'] for x in self.scraped_images]:
-                self.scraped_images.append({ 'image_url' : item['src'], 'source' : url, 'timestamp' : time.time() })
+            image_url = 'https:' + item['src']
+            if len(self.scraped_images) > 0:
+                if image_url in [x['image_url'] for x in self.scraped_images]:
+                    continue
+            self.scraped_images.append({ 'image_url' : image_url, 'source' : url, 'timestamp' : time.time() })
 
         for item in urls:
             if item['href'] not in self.explored_urls:
                 self.to_explore.add(item['href'])
 
-os.remove(SCRAPED_IMAGES_FILE)
-os.remove(EXPLORED_URLS_FILE)
-os.remove(TO_EXPLORE_FILE)
+
+
+import shutil
+
+pics_dir = os.path.join(SCRAPER_DIR, 'pics')
+
+for p in [SCRAPED_IMAGES_FILE, EXPLORED_URLS_FILE, TO_EXPLORE_FILE]:
+    try:
+        os.remove(p)
+    except Exception as e:
+        print(e)
+        pass # ;)
+shutil.rmtree(pics_dir)
+
+os.mkdir(pics_dir)
 s = Scraper()
-for i in range(100):
+
+for i in range(20):
     s.run()
     s.save()
 
-    print(f'explored: {len(s.explored_urls)}, images found: {len(s.scraped_images)}, saved urls: {len(s.to_explore)}')
+print(f'explored: {len(s.explored_urls)}, images found: {len(s.scraped_images)}, saved urls: {len(s.to_explore)}')
 
+s.download_scraped_images(pics_dir)
