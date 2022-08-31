6074cc04f672e687cdf60290130f585a45182a19
SamGheidar <sam.gheidar@gmail.com>
2022-08-31 09:44:37 +0200

    FER

--- prototypes/Sam/FER_emotion.py
+++ prototypes/Sam/FER_emotion.py
@@ -10,7 +10,7 @@ import os
 
 CURR_DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 
-input_image = cv2.imread("your pic")
+input_image = cv2.imread("happy.jpg")
 
 emotion_detector = FER()
 #emotion_detector = FER(mtcnn=True)
@@ -29,21 +29,22 @@ emotions = result[0]["emotions"]
 cv2.rectangle(input_image,(
   bounding_box[0], bounding_box[1]),(
   bounding_box[0] + bounding_box[2], bounding_box[1] + bounding_box[3]),
-              (0, 155, 255), 2)
+              (0, 155, 255), 2,)
 
 
 # Now we will add Score to Bounding Box by using the following code:
 
 
 
+
 emotion_name, score = emotion_detector.top_emotion(input_image)
 for index, (emotion_name, score) in enumerate(emotions.items()):
-   color = (211, 211,211) if score < 0.01 else (255, 10, 10)
-   emotion_score = "{}: {}".format(emotion_name, "{:.2f}".format(score))
-   org = (50, 50)
-   cv2.putText(input_image,emotion_score,
-               (bounding_box[0], bounding_box[1] + 30 + index * 15),
-               cv2.FONT_HERSHEY_SIMPLEX,0.5,color,1,cv2.LINE_AA)
+  color = (211, 211,211) if score < 0.01 else (255, 0, 0)
+  emotion_score = "{}: {}".format(emotion_name, "{:.2f}".format(score))
+
+  cv2.putText(input_image,emotion_score,
+               (bounding_box[0], bounding_box[1] + bounding_box[3] + 30 + index * 15),
+               cv2.FONT_HERSHEY_SIMPLEX,0.5,color,1,cv2.LINE_AA,)
  
 #Save the result in new image file
 cv2.imwrite("emotion.jpg", input_image)
